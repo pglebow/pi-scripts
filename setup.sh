55da80e0c47761b7936cc89209658e7c29c19722
Philip Glebow <pglebow@users.noreply.github.com>
2025-03-25 08:46:09 -0700

    pihole setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo mkdir -p /var/log/updateSystemAndPi
 sudo chown pihole:pihole /var/log/updateSystemAndPi
+sudo touch /var/log/updateSystemAndPi/updateSystemAndPi.log
+sudo chown $USER /var/log/updateSystemAndPi/updateSystemAndPi.log
 sudo mkdir /home/pihole/pi-scripts
 sudo cp $HOME/dev/pi-scripts/updateSystemAndPi.sh /home/pihole/pi-scripts/.
