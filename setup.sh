c05e375ca1470762c0c23562fecd7d55f4fa25d0
pglebow <pglebow@users.noreply.github.com>
2025-03-25 08:38:45 -0700

    pihole updates

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo mkdir -p /var/log/updateSystemAndPi
 sudo chown pihole:pihole /var/log/updateSystemAndPi
+sudo mkdir /home/pihole/pi-scripts
+sudo cp $HOME/dev/pi-scripts/updateSystemAndPi.sh /home/pihole/pi-scripts/.
--- updateSystemAndPi.sh
+++ updateSystemAndPi.sh
@@ -10,11 +10,11 @@ sudo apt update -y && sudo apt list --upgradeable && sudo apt full-upgrade -y
 
 # Update the pi-hole distribution
 printf "Updating the Pi-hole subsystem...\n"
-/usr/local/bin/pihole -up
+sudo /usr/local/bin/pihole -up
 
 # Update the blocking database
 printf "Updating the gravity database in Pi-hole...\n"
-/usr/local/bin/pihole -g
+sudo /usr/local/bin/pihole -g
 
 now=$(date)
 printf "\n------------------------------------------\n"
